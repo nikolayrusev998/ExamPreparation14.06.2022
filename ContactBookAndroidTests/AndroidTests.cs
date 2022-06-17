fec01edb6aa3b4dfd09b9eb2ef8a3f7889567d4c
Brrratski-PC <nikolay.rusev.main@gmail.com>
2022-06-17 16:01:14 +0300

    Exam preparation for QA Automation

--- ContactBookAndroidTests/AndroidTests.cs
+++ ContactBookAndroidTests/AndroidTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using System;
@@ -7,34 +8,54 @@ namespace ContactBookAndroidTests
 {
     public class AndroidTests
     {
-
-        private AndroidDriver <AndroidElement> driver;
-        private AppiumOptions options;
         private const string AppiumUrl = "http://127.0.0.1:4723/wd/hub";
-        private const string ContactsBookURL = "https://contactbook.nikolayrusev1.repl.co/";
-        private const string location = @"D:\contactbook-androidclient.apk";
+        private const string ContactsBookUrl = "https://contactbook.nakov.repl.co/api";
+        private const string appLocation = @"C:\contactbook-androidclient.apk";
 
+        private AndroidDriver<AndroidElement> driver;
+        private AppiumOptions options;
 
         [SetUp]
         public void StartApp()
         {
-            options = new AppiumOptions() { PlatformName = "Android"};
-            options.AddAdditionalCapability("app", location);
+            options = new AppiumOptions() { PlatformName = "Android" };
+            options.AddAdditionalCapability("app", appLocation);
 
-            driver = new AndroidDriver<AndroidElement>(new Uri(AppiumUrl) ,options);
+            driver = new AndroidDriver<AndroidElement>(new Uri(AppiumUrl), options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
         }
+
+
         [TearDown]
-        public void Close()
+        public void CloseApp()
         {
             driver.Quit();
-
         }
 
         [Test]
-        public void Test1()
+        public void Test_SearchContact_VerifyFirstResult()
         {
-            Assert.Pass();
+            // Arrange
+            var urlField = driver.FindElement(By.Id("contactbook.androidclient:id/editTextApiUrl"));
+            urlField.Clear();
+            urlField.SendKeys(ContactsBookUrl);
+
+            var buttonConnect = driver.FindElement(By.Id("contactbook.androidclient:id/buttonConnect"));
+            buttonConnect.Click();
+
+            var editTextField = driver.FindElement(By.Id("contactbook.androidclient:id/editTextKeyword"));
+            editTextField.SendKeys("steve");
+
+            //Act
+            var buttonSearch = driver.FindElement(By.Id("contactbook.androidclient:id/buttonSearch"));
+            buttonSearch.Click();
+
+            // Assert
+            var firstName = driver.FindElement(By.XPath("//android.widget.TableRow[3]/android.widget.TextView[2]"));
+            var lastName = driver.FindElement(By.XPath("//android.widget.TableRow[4]/android.widget.TextView[2]"));
+
+            Assert.That(firstName.Text, Is.EqualTo("Steve"));
+            Assert.That(lastName.Text, Is.EqualTo("Jobs"));
         }
     }
 }
